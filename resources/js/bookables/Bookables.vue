b19825bbc6dfb01f2bfb793555d5123757bbebcc
Valerio <valerio.modesti@gmail.com>
2020-12-16 14:54:08 +0100

    props

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <bookable-list-item title="Cheap Villa" content="a very cheap villa" :price="1000"></bookable-list-item>
+        <bookable-list-item></bookable-list-item>
+        <bookable-list-item></bookable-list-item>
+        <bookable-list-item></bookable-list-item>
+    </div>
+</template>
+
+<script>
+import BookableListItem from "./BookableListItem";
+export default {
+   components: {
+       BookableListItem   
+   } 
+}
+</script>
